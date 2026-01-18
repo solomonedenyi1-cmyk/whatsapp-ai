7ca288299139a7ace47b99b63b1c50f6ce6127b9
Yuri Cunha <isyuricunha@duck.com>
2026-01-18 01:08:18 +0000

    test(whatsapp): add waitForClientReady unit tests

--- jest.config.js
+++ jest.config.js
@@ -1,35 +1,36 @@
 module.exports = {
   // Test environment
   testEnvironment: 'node',
-  
+
   // Test match pattern
   testMatch: ['**/test/**/*.test.js'],
-  
+
   // Coverage settings
   collectCoverage: true,
   collectCoverageFrom: [
     'src/**/*.js',
+    '!src/**/__tests__/**',
     '!src/**/node_modules/**',
     '!src/**/vendor/**'
   ],
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov'],
-  
+
   // Timeout settings
   testTimeout: 10000,
-  
+
   // Verbose output
   verbose: true,
-  
+
   // Setup files
   setupFilesAfterEnv: [],
-  
+
   // Clear mocks between tests
   clearMocks: true,
-  
+
   // Restore mocks between tests
   restoreMocks: true,
-  
+
   // Reset modules between tests
   resetModules: false
 };
