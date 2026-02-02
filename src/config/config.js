d0674cdaa5f084604dce08aa162303f754ac661e
Yuri Cunha <isyuricunha@duck.com>
2026-02-02 00:59:04 +0000

    fix(docker): stabilize puppeteer and env parsing in local compose

--- src/config/config.js
+++ src/config/config.js
@@ -112,7 +112,6 @@ const config = {
         '--disable-accelerated-2d-canvas',
         '--no-first-run',
         '--no-zygote',
-        '--single-process',
         '--disable-gpu'
       ]
     }
